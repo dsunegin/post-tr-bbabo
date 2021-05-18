0d7cd76d79fbe5370c68d986839bf43e5bf487a9
Denis Sunegin <dsunegin@gmail.com>
2021-05-18 21:37:06 +0100

    Initial commit

--- pm2-post-tr-bbabo.sh
+++ pm2-post-tr-bbabo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Start each 1 min
+# Start each 2 min
 
 pm2 delete post-edaru
-CRON="* * * * *" pm2 start npm --name post-tr-bbabo  -- run start
+CRON="*/2 * * * *" pm2 start npm --name post-tr-bbabo  -- run start
 pm2 save
